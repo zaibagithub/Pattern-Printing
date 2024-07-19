ecb0fe83255cdc02ae2910540365e9ab8f9d6c31
ZAIBA THABASSUM K <133732408+zaibagithub@users.noreply.github.com>
2024-07-19 08:50:00 +0530

    Create Hollow inverted pyramid .java

---
+++ Hollow inverted pyramid .java
@@ -0,0 +1,27 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        	int n=5;
+		for(int i=n;i>=1;i--){
+		    for(int j=n;j>i;j--){
+		        System.out.print("  ");
+		    }
+		    for(int j=1;j<=(2*i-1);j++){
+		        if( i==n || j==1|| j== (2*i-1)){
+		            System.out.print("* ");
+		        }
+		        else{
+		            System.out.print("  ");
+		        }
+		    }
+		    System.out.println();
+		}
+    }
+}
+
+OUTPUT:
+
+* * * * * * * * * 
+  *           * 
+    *       * 
+      *   * 
+        * 
